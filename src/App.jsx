60ee66e8b809422026f61beec9a93b6af4067487
Anu025 <anu001125@gmail.com>
2025-03-06 00:04:00 +0530

    deploy react to github

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>First Project to Deploy on Github</h1>
+    </>
+  )
+}
+
+export default App
